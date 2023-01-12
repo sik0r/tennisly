86f6f9b4097ac25b4aecc25c56eaabfa769578fe
sik0r <dsikorski40@gmail.com>
2023-01-12 11:05:20 +0100

    require easyadmin

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
